2a8747d2f057ff2e2955e3430091a2f2f5d1423a
yeonjy <yeonjypaper@gmail.com>
2025-04-05 14:40:26 +0900

    #1 feat: 예외처리 설정

---
+++ src/main/java/com/webeye/backend/global/error/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.webeye.backend.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    ;
+    private final HttpStatus status;
+    private final String errorMessage;
+}
