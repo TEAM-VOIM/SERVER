4e490969fac8679dd366e0eb034a554663ea5f8c
zyovn <yui2507@naver.com>
2025-05-01 02:50:24 +0900

    #23 feat: clova x client

--- src/main/java/com/webeye/backend/review/infrastructure/clovaX/ClovaXClient.java
+++ src/main/java/com/webeye/backend/review/infrastructure/clovaX/ClovaXClient.java
@@ -1,4 +1,24 @@
 package com.webeye.backend.review.infrastructure.clovaX;
 
-public class ClovaXClient {
+import com.webeye.backend.global.config.OpenFeignConfig;
+import com.webeye.backend.review.dto.response.ReviewResponse;
+import com.webeye.backend.review.infrastructure.clovaX.dto.request.ClovaXRequest;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(
+        name = "clovaXClient",
+        url = "${clova.url}",
+        configuration = OpenFeignConfig.class
+)
+public interface ClovaXClient {
+
+    @PostMapping()
+    ReviewResponse createReviewSummary(
+            @RequestHeader("Authorization") String authorization,
+            @RequestHeader("X-NCP-CLOVASTUDIO-REQUEST-ID") String requestId,
+            @RequestHeader("Content-Type") String contentType,
+            @RequestBody @Valid ClovaXRequest request
+    );
 }
