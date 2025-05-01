70b9a9be7db6fbaa593d6d31c9d5bc4088843765
yeonjy <yeonjypaper@gmail.com>
2025-05-02 00:08:39 +0900

    #27 feat: 음식 상품 알레르기 유발 성분 저장

--- src/main/java/com/webeye/backend/allergy/application/AllergyService.java
+++ src/main/java/com/webeye/backend/allergy/application/AllergyService.java
@@ -1,18 +1,37 @@
 package com.webeye.backend.allergy.application;
 
-import com.webeye.backend.allergy.dto.response.AllergyResponse;
-import com.webeye.backend.imageanalysis.dto.request.ImageAnalysisRequest;
+import com.webeye.backend.allergy.dto.response.AllergyAiResponse;
+import com.webeye.backend.product.dto.request.ProductAnalysisRequest;
 import com.webeye.backend.imageanalysis.infrastructure.OpenAiClient;
+import com.webeye.backend.product.domain.Product;
+import com.webeye.backend.product.domain.ProductAllergy;
+import com.webeye.backend.product.persistent.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AllergyService {
     private final OpenAiClient openAiClient;
+    private final ProductRepository productRepository;
 
-    public AllergyResponse analyzeAllergy(ImageAnalysisRequest request) {
+    public AllergyAiResponse analyzeAllergy(ProductAnalysisRequest request) {
         return openAiClient.explainAllergy(request);
     }
 
+    @Transactional
+    public void saveProductAllergy(Product product, ProductAnalysisRequest request) {
+        AllergyAiResponse response = analyzeAllergy(request);
+
+        response.getAllergyTypes().forEach(allergyType ->
+                product.addAllergy(
+                        ProductAllergy.builder()
+                                .product(product)
+                                .allergy(allergyType)
+                                .build()
+                )
+        );
+        productRepository.save(product);
+    }
 }
