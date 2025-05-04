e69452dd2d56cf1be5596e5265d5e4dfe1277763
yeonjy <yeonjypaper@gmail.com>
2025-05-05 00:26:14 +0900

    #32 docs: Nutrient 한국어 이름 swagger에 추가

--- src/main/java/com/webeye/backend/product/dto/response/ProductResponse.java
+++ src/main/java/com/webeye/backend/product/dto/response/ProductResponse.java
@@ -7,7 +7,7 @@ import lombok.Builder;
 
 import java.util.List;
 
-@Schema(description = "제품 분석 응답")
+@Schema(description = "제품 분석 응답 (SODIUM: 나트륨, CARBOHYDRATE: 탄수화물, SUGARS: 당류, FAT: 지방, TRANS_FAT: 트랜스지방, SATURATED_FAT: 포화지방, CHOLESTEROL: 콜레스테롤, PROTEIN: 단백질, CALCIUM: 칼슘, PHOSPHORUS: 인, NIACIN: 나이아신, VITAMIN_B: 비타민 B, VITAMIN_E: 비타민 E)")
 @Builder
 public record ProductResponse (
         @Schema(description = "포함된 주의 영양성분")
