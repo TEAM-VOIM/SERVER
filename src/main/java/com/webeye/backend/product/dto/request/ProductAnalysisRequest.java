71405d5f6c7f3ff55bfd0fb2add684e798c05163
yeonjy <yeonjypaper@gmail.com>
2025-05-08 21:45:57 +0900

    #32 feat: 식품 분석 기능 - 영양성분 분석 추가

--- src/main/java/com/webeye/backend/product/dto/request/ProductAnalysisRequest.java
+++ src/main/java/com/webeye/backend/product/dto/request/ProductAnalysisRequest.java
@@ -1,6 +1,7 @@
 package com.webeye.backend.product.dto.request;
 
 import com.webeye.backend.allergy.type.AllergyType;
+import com.webeye.backend.nutrition.domain.type.Gender;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
@@ -15,6 +16,12 @@ public record ProductAnalysisRequest(
 
         @NotEmpty(message = "이미지 URL 목록은 비어있을 수 없습니다.")
         List<String> urls,
+
+        @Schema(description = "사용자 출생년도")
+        int birthYear,
+
+        @Schema(description = "사용자 성별")
+        Gender gender,
 
         @Schema(description = "사용자 알레르기")
         List<AllergyType> allergies
