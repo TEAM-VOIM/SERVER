fc1b14333eb1feac54b03057ecea3c2595c2c2c4
yeonjy <yeonjypaper@gmail.com>
2025-05-08 21:41:44 +0900

    #32 feat: 영양성분 권장량의 40% 넘는 식품만 반환

---
+++ src/main/java/com/webeye/backend/nutrition/application/NutrientRecommendationService.java
@@ -0,0 +1,51 @@
+package com.webeye.backend.nutrition.application;
+
+import com.webeye.backend.nutrition.domain.Nutrient;
+import com.webeye.backend.nutrition.domain.NutrientRecommendation;
+import com.webeye.backend.nutrition.dto.request.NutrientRecommendationRequest;
+import com.webeye.backend.nutrition.dto.response.NutrientRecommendationResponse;
+import com.webeye.backend.nutrition.persistent.NutrientRecommendationRepository;
+import com.webeye.backend.product.domain.ProductNutrient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Year;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NutrientRecommendationService {
+    private final NutrientRecommendationRepository recommendationRepository;
+
+    @Transactional
+    public List<NutrientRecommendationResponse> analyzeNutrientSufficiency(NutrientRecommendationRequest request) {
+        List<NutrientRecommendationResponse> overRecommendationNutrients = new LinkedList<>();
+        int userAge = Year.now().getValue() - request.birthYear();
+
+        for (ProductNutrient productNutrient : request.product().getNutrients()) {
+            Nutrient nutrient = productNutrient.getNutrient();
+
+            Optional<NutrientRecommendation> recommendationOpt = recommendationRepository
+                    .findByNutrientIdAndGenderAndAge(nutrient.getId(), request.gender(), userAge);
+
+            if (recommendationOpt.isEmpty()) {
+                continue;
+            }
+
+            NutrientRecommendation recommendation = recommendationOpt.get();
+            double percentage = productNutrient.getAmount() / recommendation.getAmount() * 100;
+
+            if (percentage >= 40.0) {
+                overRecommendationNutrients.add(NutrientRecommendationResponse.builder()
+                        .nutrientType(productNutrient.getNutrient().getType())
+                        .percnetage((int) percentage).build());
+            }
+        }
+        return overRecommendationNutrients;
+    }
+}
