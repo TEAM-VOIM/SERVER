1175f17d3df6a9b17fcee7b58576c90602ce3c16
yeonjy <yeonjypaper@gmail.com>
2025-05-08 22:53:47 +0900

    #32 feat: 사용자 정보 비어있는지 검증

--- src/main/java/com/webeye/backend/product/dto/request/ProductAnalysisRequest.java
+++ src/main/java/com/webeye/backend/product/dto/request/ProductAnalysisRequest.java
@@ -14,13 +14,16 @@ public record ProductAnalysisRequest(
         @NotEmpty(message = "제품 ID는 비어있을 수 없습니다.")
         String productId,
 
+        @Schema(description = "상품 이미지 URL")
         @NotEmpty(message = "이미지 URL 목록은 비어있을 수 없습니다.")
         List<String> urls,
 
         @Schema(description = "사용자 출생년도")
+        @NotEmpty(message = "사용자의 출생연도는 비어있을 수 없습니다.")
         int birthYear,
 
         @Schema(description = "사용자 성별")
+        @NotEmpty(message = "사용자의 성별은 비어있을 수 없습니다.")
         Gender gender,
 
         @Schema(description = "사용자 알레르기")
