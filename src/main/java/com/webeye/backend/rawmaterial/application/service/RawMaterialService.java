00cd4d7cf78eecea888aaf24a75c0f1fffcb8c3f
yeonjy <yeonjypaper@gmail.com>
2025-05-12 22:54:57 +0900

    #36 remove: 기존 Open API 호출해서 원재료 영양성분 받아오는 코드 제거

--- src/main/java/com/webeye/backend/rawmaterial/application/service/RawMaterialService.java
+++
@@ -1,7 +0,0 @@
-package com.webeye.backend.rawmaterial.application.service;
-
-import com.webeye.backend.rawmaterial.dto.RawMaterialResponse;
-
-public interface RawMaterialService {
-    RawMaterialResponse.Body callRawMaterialAPI(Integer pageNo, Integer numOfRows);
-}
