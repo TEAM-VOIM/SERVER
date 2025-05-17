50f985dcc8766c3e6cd9965e043cbc30278a2be1
zyovn <yui2507@naver.com>
2025-05-17 19:00:46 +0900

    #50 docs: schema 작성

--- src/main/java/com/webeye/backend/review/dto/request/ReviewSummaryRequest.java
+++ src/main/java/com/webeye/backend/review/dto/request/ReviewSummaryRequest.java
@@ -8,6 +8,21 @@ import java.util.Map;
 @Builder
 @Schema(description = "리뷰 목록")
 public record ReviewSummaryRequest(
+        @Schema(description = "리뷰 만족도 통계",
+                example = """
+                {
+                  "맛 만족도": {
+                    "맛있어요": 55,
+                    "보통이에요": 16,
+                    "생각보다 별로예요": 29
+                  },
+                  "당도": {
+                    "아주 달콤해요": 39,
+                    "적당해요": 29,
+                    "달지 않아요": 32
+                  }
+                }
+                """)
         Map<String, Map<String, Integer>> reviews
 ) {
 }
