014d0316ae7e6be89dbd9b55362f0a496c3fece1
yeonjy <yeonjypaper@gmail.com>
2025-05-17 18:32:20 +0900

    #49 docs: swagger와 오류 메시지 수정

--- src/main/java/com/webeye/backend/product/dto/request/ProductDetailAnalysisRequest.java
+++ src/main/java/com/webeye/backend/product/dto/request/ProductDetailAnalysisRequest.java
@@ -7,8 +7,8 @@ import lombok.Builder;
 @Schema(description = "상품 설명 이미지의 HTML")
 @Builder
 public record ProductDetailAnalysisRequest(
-        @Schema(description = "상품 상세 이미지")
-        @NotEmpty(message = "이미지 URL 목록은 비어있을 수 없습니다.")
+        @Schema(description = "상품 상세 정보 HTML")
+        @NotEmpty(message = "상품 상세 정보의 HTML은 비어있을 수 없습니다.")
         String html
 ){
 }
