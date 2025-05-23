713b0cd84b4c9f7791c065d6dade6f67030185c6
zyovn <yui2507@naver.com>
2025-05-23 18:20:23 +0900

    #65 feat: ingredientInit 설계

--- src/main/java/com/webeye/backend/cosmetic/persistence/IngredientRepository.java
+++ src/main/java/com/webeye/backend/cosmetic/persistence/IngredientRepository.java
@@ -1,7 +1,11 @@
 package com.webeye.backend.cosmetic.persistence;
 
 import com.webeye.backend.cosmetic.domain.Ingredient;
+import com.webeye.backend.cosmetic.domain.type.IngredientType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+    Optional<Ingredient> findByIngredientType(IngredientType ingredientType);
 }
---
+++ src/main/java/com/webeye/backend/cosmetic/persistence/init/IngredientInit.java
@@ -0,0 +1,34 @@
+package com.webeye.backend.cosmetic.persistence.init;
+
+import com.webeye.backend.cosmetic.domain.Ingredient;
+import com.webeye.backend.cosmetic.domain.type.IngredientType;
+import com.webeye.backend.cosmetic.persistence.IngredientRepository;
+import com.webeye.backend.global.util.DummyDataInit;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+
+@Slf4j
+@RequiredArgsConstructor
+@Order(1)
+@DummyDataInit
+public class IngredientInit implements ApplicationRunner {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Builder
+    public void run(ApplicationArguments args) {
+        for (IngredientType ingredientType : IngredientType.values()) {
+            if (ingredientRepository.findByIngredientType(ingredientType).isEmpty()) {
+                Ingredient ingredient = Ingredient.builder()
+                        .ingredientType(ingredientType)
+                        .build();
+
+                ingredientRepository.save(ingredient);
+            }
+        }
+    }
+}
