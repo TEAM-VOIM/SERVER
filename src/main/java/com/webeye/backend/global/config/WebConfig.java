52b0f704715ba0061dc11ebcfa83869bed3e2e2f
zyovn <yui2507@naver.com>
2025-05-24 16:55:30 +0900

    #76 refactor: 수정

--- src/main/java/com/webeye/backend/global/config/WebConfig.java
+++ src/main/java/com/webeye/backend/global/config/WebConfig.java
@@ -16,10 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                         "chrome-extension://iofbhhcbidmfcmpjndglaignlfdojpcm",
                         "chrome-extension://jeppkpjgeheckphiogogbffdenhlkclh",
                         "chrome-extension://ecbaebehchfclbcglpabiclbgkjoihmf",
-                        "chrome-extension://aphlbfidmnkcjfanjfjhbfldlpgndgac",
-                        "chrome-extension://ghplnadekipfddbmbpcpkophjobfpkkj",
-                        "chrome-extension://hemleepcpkkmkapaliflaohhnnapfdlh",
-                        "chrome-extension://ehgaglekgllijnoglmdfeingpecfjbmb"
+                        "chrome-extension://aphlbfidmnkcjfanjfjhbfldlpgndgac"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
