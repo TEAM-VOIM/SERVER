3a2eeeadff9eb5b344a095456cc61120efa2e31b
yeonjy <yeonjypaper@gmail.com>
2025-05-26 18:58:12 +0900

    #84 feat: productId @NotBlank 추가

--- src/main/java/com/webeye/backend/product/dto/request/ProductDetailAnalysisRequest.java
+++ src/main/java/com/webeye/backend/product/dto/request/ProductDetailAnalysisRequest.java
@@ -1,6 +1,7 @@
 package com.webeye.backend.product.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.Builder;
 @Builder
 public record ProductDetailAnalysisRequest(
         @Schema(description = "상품 ID")
+        @NotBlank(message = "상품 ID는 비어있을 수 없습니다.")
         String productId,
 
         @Schema(description = "상품 상세 정보 HTML")
