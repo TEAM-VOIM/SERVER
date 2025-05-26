f4d80a9e0ddb8503b808f93aaab14bc7a07694ec
yeonjy <yeonjypaper@gmail.com>
2025-05-26 18:08:43 +0900

    #84 remove: 사용하지 않는 API 제거

--- src/main/java/com/webeye/backend/product/presentation/ProductController.java
+++ src/main/java/com/webeye/backend/product/presentation/ProductController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import static com.webeye.backend.global.dto.response.type.SuccessCode.FOOD_PRODUCT_ANALYSIS_SUCCESS;
-import static com.webeye.backend.global.dto.response.type.SuccessCode.PRODUCT_DETAIL_EXPLANATION_ANALYSIS_SUCCESS;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,13 +26,5 @@ public class ProductController implements ProductSwagger {
     @PostMapping(value = "/foods")
     public SuccessResponse<ProductResponse> foodAnalysis(@Valid @RequestBody FoodProductAnalysisRequest request) {
         return SuccessResponse.of(FOOD_PRODUCT_ANALYSIS_SUCCESS, productService.analyzeFoodProduct(request));
-    }
-
-    @Override
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping(value = "/analysis/{outline}")
-    public SuccessResponse<DetailExplanationResponse> productDetailAnalysis(
-            @PathVariable OutlineType outline, @Valid @RequestBody ProductDetailAnalysisRequest request) {
-        return SuccessResponse.of(PRODUCT_DETAIL_EXPLANATION_ANALYSIS_SUCCESS, productService.analyzeProductDetail(outline, request));
     }
 }
--- src/main/java/com/webeye/backend/product/presentation/swagger/ProductSwagger.java
+++ src/main/java/com/webeye/backend/product/presentation/swagger/ProductSwagger.java
@@ -29,21 +29,4 @@ public interface ProductSwagger {
     SuccessResponse<ProductResponse> foodAnalysis(
             @RequestBody FoodProductAnalysisRequest request
     );
-
-    @Operation(
-            summary = "제품 주요 개요에 대한 상세 설명 추출",
-            description = "제품 상세 설명 이미지가 포함된 HTML과 개요를 입력받아 주요 요소에 대한 상세 설명을 추출합니다. " +
-                    "MAIN: 주요정보, USAGE: 사용정보, WARNING: 주의 및 보관, SPECS: 규격 및 옵션, CERTIFICATION: 인증 및 기타"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "제품 주요 요소에 대한 상세 설명이 추출되었습니다."
-            )
-    })
-    SuccessResponse<DetailExplanationResponse> productDetailAnalysis(
-            @Parameter(in = ParameterIn.PATH, description = "상품 개요", required = true)
-            OutlineType outline,
-            @RequestBody ProductDetailAnalysisRequest request
-    );
 }
