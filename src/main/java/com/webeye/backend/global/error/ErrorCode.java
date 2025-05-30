0a2f0f8216a971630150cf8c01121afcf399eb37
yeonjy <yeonjypaper@gmail.com>
2025-05-31 01:42:47 +0900

    #134 fix: 이미지 확장자가 uri의 마지막이 아닌 경우 trim 처리

--- src/main/java/com/webeye/backend/global/error/ErrorCode.java
+++ src/main/java/com/webeye/backend/global/error/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     FILE_EXTENSION_NOT_FOUND(HttpStatus.BAD_REQUEST, "URL에서 확장자를 찾을 수 없습니다."),
     UNSUPPORTED_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 이미지 형식입니다."),
     INVALID_IMAGE_URL(HttpStatus.BAD_REQUEST, "잘못된 이미지 URL입니다."),
+    NO_IMAGE_FILE_EXTENSION_FOUND(HttpStatus.BAD_REQUEST, "이미지 확장자가 없습니다."),
 
     // image url extract
     IMAGE_URL_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지의 URL이 추출되지 않았습니다."),
